5774a92cb1e362409bb682b1f19b3105b5758128
Tucker Siegel <tgsiegel@umd.edu>
2025-06-02 23:08:18 -0400

    update

--- src/pin_pulser.rs
+++ src/pin_pulser.rs
@@ -1,10 +1,13 @@
 use std::{thread::sleep, time::Duration};
 
 use crate::{
-    gpio_bits, registers::{ClkRegisters, GPIOFunction, GPIORegisters, PWMRegisters, TimeRegisters}
+    gpio_bits,
+    registers::{ClkRegisters, GPIOFunction, GPIORegisters, PWMRegisters, TimeRegisters},
 };
 
 const PWM_BASE_TIME_NS: u32 = 2;
+const EMPIRICAL_NANOSLEEP_OVERHEAD_US: u32 = 12;
+const MINIMUM_NANOSLEEP_TIME_US: u32 = 5;
 
 struct Pulse {
     start_time: u64,
@@ -13,10 +16,10 @@ struct Pulse {
 
 pub(crate) trait PinPulser {
     fn send_pulse(
-        &mut self, 
+        &mut self,
         bitplane: usize,
         pwm_registers: &mut PWMRegisters,
-        time_registers: &mut TimeRegisters
+        time_registers: &mut TimeRegisters,
     );
     fn wait_pulse_finished(
         &mut self,
@@ -26,44 +29,59 @@ pub(crate) trait PinPulser {
 }
 
 pub(crate) struct HardwarePinPulser {
-    /// Hints how long to sleep.
     sleep_hints_us: Vec<u32>,
-    /// Pulse period for each bit plane.
     pulse_periods: Vec<u32>,
-    /// The current pulse.
     current_pulse: Option<Pulse>,
 }
 
 pub(crate) struct TimerBasedPinPulser {
-    sleep_hints_us: Vec<u32>,
+    sleep_hints_ns: Vec<u64>,
 }
 
 impl TimerBasedPinPulser {
     pub fn new(bitplane_timings_ns: &[u32]) -> Self {
-        let sleep_hints_us = bitplane_timings_ns.iter().map(|t| t / 1000).collect();
-        TimerBasedPinPulser { sleep_hints_us }
+        let sleep_hints_ns = bitplane_timings_ns
+            .iter()
+            .map(|&t| t.saturating_sub(EMPIRICAL_NANOSLEEP_OVERHEAD_US * 1000) as u64)
+            .collect();
+        TimerBasedPinPulser { sleep_hints_ns }
+    }
+
+    fn sleep_precise(&self, time_registers: &TimeRegisters, duration_ns: u64) {
+        let min_total_ns = (EMPIRICAL_NANOSLEEP_OVERHEAD_US + MINIMUM_NANOSLEEP_TIME_US) * 1000;
+
+        if duration_ns > min_total_ns.into() {
+            let sleep_ns = duration_ns - ((EMPIRICAL_NANOSLEEP_OVERHEAD_US as u64) * 1000);
+            sleep(Duration::from_nanos(sleep_ns));
+        }
+
+        let start_us = time_registers.get_time();
+        let target_us = start_us + duration_ns / 1000;
+        while time_registers.get_time() < target_us {
+            std::hint::spin_loop();
+        }
     }
 }
 
 impl PinPulser for TimerBasedPinPulser {
     fn send_pulse(
-        &mut self, 
+        &mut self,
         bitplane: usize,
-        pwm_registers: &mut PWMRegisters,
-        time_registers: &mut TimeRegisters
+        _pwm_registers: &mut PWMRegisters,
+        time_registers: &mut TimeRegisters,
     ) {
-        sleep(Duration::from_micros(self.sleep_hints_us[bitplane] as u64));
+        let ns = self.sleep_hints_ns[bitplane];
+        self.sleep_precise(time_registers, ns);
     }
 
     fn wait_pulse_finished(
         &mut self,
-        time_registers: &mut TimeRegisters,
-        pwm_registers: &mut PWMRegisters,
+        _time_registers: &mut TimeRegisters,
+        _pwm_registers: &mut PWMRegisters,
     ) {
-        
+        // No-op for timer-based pulse
     }
 }
-
 
 impl HardwarePinPulser {
     pub(crate) fn new(
@@ -78,10 +96,8 @@ impl HardwarePinPulser {
         let time_base = bitplane_timings_ns[0];
 
         if pins == gpio_bits!(18) {
-            // Set GPIO 18 to PWM0 mode
             gpio_registers.select_function(18, GPIOFunction::Alt5);
         } else if pins == gpio_bits!(12) {
-            // Set GPIO 12 to PWM0 mode
             gpio_registers.select_function(12, GPIOFunction::Alt0);
         } else {
             unreachable!()
@@ -89,6 +105,7 @@ impl HardwarePinPulser {
 
         pwm_registers.reset_pwm();
         clk_registers.init_pwm_divider((time_base / 2) / PWM_BASE_TIME_NS);
+
         let pulse_periods = bitplane_timings_ns
             .iter()
             .map(|timing| 2 * timing / time_base)
@@ -113,9 +130,6 @@ impl PinPulser for HardwarePinPulser {
             pwm_registers.set_pwm_pulse_period(self.pulse_periods[bitplane]);
             pwm_registers.push_fifo(self.pulse_periods[bitplane]);
         } else {
-            // Keep the actual range as short as possible, as we have to wait for one full period of these in
-            // the zero phase. The hardware can't deal with values < 2, so only do this when we have have
-            // enough of these.
             let period_fraction = self.pulse_periods[bitplane] / 8;
             pwm_registers.set_pwm_pulse_period(period_fraction);
             for _ in 0..8 {
@@ -123,20 +137,14 @@ impl PinPulser for HardwarePinPulser {
             }
         }
 
-        // We need one sentinel value at the end to have it go back to default state (otherwise it just
-        // repeats the last value, so will be constantly 'on').
         pwm_registers.push_fifo(0);
-
-        // For some reason, we need a second empty sentinel in the FIFO, otherwise our way to detect the end
-        // of the pulse, which relies on "is the queue empty" does not work. It is not entirely clear why that
-        // is from the data sheet, but probably there is some buffering register in which data elements are
-        // kept after the FIFO is emptied.
         pwm_registers.push_fifo(0);
 
         self.current_pulse = Some(Pulse {
             start_time: time_registers.get_time(),
             sleep_hint_us: self.sleep_hints_us[bitplane],
         });
+
         pwm_registers.enable_pwm();
     }
 
@@ -154,7 +162,6 @@ impl PinPulser for HardwarePinPulser {
         time_registers.sleep_at_most(remaining_time_us);
 
         while !pwm_registers.fifo_empty() {
-            // busy wait until done.
             std::thread::yield_now();
         }
 
